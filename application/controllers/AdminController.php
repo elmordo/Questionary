3cf8ea54b354c2eff4cde88e82208b0cc1a0a7fa
Bc. Petr Jindra <el.mordo@gmail.com>
2012-11-10 14:31:11 +0100

    Vytvoren administratorsky kontroler a hlavicky jeho metod

---
+++ application/controllers/AdminController.php
@@ -0,0 +1,59 @@
+<?php
+class AdminController extends Zend_Controller_Action {
+	
+	/**
+	 * inicializace dat
+	 */
+	public function init() {
+		
+	}
+	
+	/**
+	 * zobrazi vyplneny dotaznik
+	 */
+	public function filledAction() {
+		
+	}
+	
+	/**
+	 * vypis vyplnenych dotazniku jednoho druhu
+	 */
+	public function filledlistAction() {
+		
+	}
+	
+	/**
+	 * vypis menu
+	 */
+	public function indexAction() {
+		
+	}
+	
+	/**
+	 * vypis formulare pro login
+	 */
+	public function loginAction() {
+		
+	}
+	
+	/**
+	 * vypis sablon dotazniku
+	 */
+	public function qlistAction() {
+		
+	}
+	
+	/**
+	 * zalogovani uzivatele
+	 */
+	public function signinAction() {
+		
+	}
+	
+	/**
+	 * odhlaseni uzivatele
+	 */
+	public function signoutAction() {
+		
+	}
+}
