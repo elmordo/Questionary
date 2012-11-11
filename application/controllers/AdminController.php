b641e2f43df2cd9a86e3eb488aa25ca7242ce6df
Bc. Petr Jindra <el.mordo@gmail.com>
2012-11-11 16:23:20 +0100

    Vytvoren autentizacni mechanismus Pridany metody pro vytvoreni a upravu dotazniku

--- application/controllers/AdminController.php
+++ application/controllers/AdminController.php
@@ -2,10 +2,41 @@
 class AdminController extends Zend_Controller_Action {
 	
 	/**
-	 * inicializace dat
+	 * @var Application_Model_Row_User
+	 */
+	protected $_user = null;
+	
+	/**
+	 * @var Application_Model_Users
+	 */
+	protected $_tableUsers;
+	
+	/**
+	 * @var Application_Model_Questionaries
+	 */
+	protected $_tableQuestionaries;
+	
+	/**
+	 * @var Zend_Session_Namespace
+	 */
+	protected $_session;
+	
+	/**
+	 * inicializuje instanci
 	 */
 	public function init() {
+		// priprava tabulek
+		$this->_tableQuestionaries = new Application_Model_Questionaries;
+		$this->_tableUsers = new Application_Model_Users;
 		
+		// kontrola prihlaseneho uzivatele
+		$session = new Zend_Session_Namespace("admin");
+		$this->_session = $session;
+		
+		if ($session->user_id) {
+			// nacteni a zapis uzivatele
+			$this->_user = $this->_tableUsers->find($session->user_id)->current();
+		}
 	}
 	
 	/**
@@ -19,6 +50,13 @@ class AdminController extends Zend_Controller_Action {
 	 * smaze uzivatele
 	 */
 	public function deleteuserAction() {
+		
+	}
+	
+	/**
+	 * editace sablony dotazniku
+	 */
+	public function editqAction() {
 		
 	}
 	
@@ -58,9 +96,23 @@ class AdminController extends Zend_Controller_Action {
 	}
 	
 	/**
+	 * vytvori novy dotaznik
+	 */
+	public function postqAction() {
+		
+	}
+	
+	/**
 	 * vytvori uzivatele
 	 */
 	public function postuserAction() {
+		
+	}
+	
+	/**
+	 * ulozi dotaznik
+	 */
+	public function putqJsonAction() {
 		
 	}
 	
@@ -82,13 +134,63 @@ class AdminController extends Zend_Controller_Action {
 	 * zalogovani uzivatele
 	 */
 	public function signinAction() {
+		// nacteni dat
+		$data = $this->getRequest()->getParam("user", array());
+		$data = array_merge(array("login" => "", "password" => ""), $data);
 		
+		// kontrola dat
+		$user = null;
+		try {
+			$user = $this->_tableUsers->findByLogin($data["login"]);
+			
+			// kontrola nalezeni
+			if (!$user) throw new Zend_Exception("User not found");
+			
+			// kontrola hesla
+			if (!$user->checkPassword($data["password"])) throw new Zend_Exception("Incorect password");
+		} catch (Zend_Exception $e) {
+			die($e->getMessage());
+			$this->_forward("login");
+			return;
+		}
+		
+		// nastaveni uzivatele
+		$this->_session->user_id = $user->id;
+		
+		// presmerovani na index
+		$this->_redirect("/admin/index");
 	}
 	
 	/**
 	 * odhlaseni uzivatele
 	 */
 	public function signoutAction() {
+		// odebrani informaci o uzivatelu
+		$this->_session->user_id = null;
 		
+		// presmerovani na login
+		$this->_redirect("/admin/login");
 	}
+	
+	/*
+	 * vyhodnoceni pristupove pravo
+	 */
+	 public function preDispatch() {
+	 	// vyhodnoceni akci a podobne
+	 	if (!$this->_user) {
+	 		// uzivatel neni prihlasen - zkontroluje se akce
+	 		$action = strtolower($this->getRequest()->getActionName());
+			
+	 		switch ($action) {
+	 			case "login":
+	 			case "signin":
+					// verejne akce
+					break;
+					
+				default:
+					// zobrazi se login
+					$this->getRequest()->setActionName("login");
+	 		}
+	 	}
+	 }
 }
